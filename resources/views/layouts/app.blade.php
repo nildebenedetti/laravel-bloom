265dc2a7063dd17e1708ef6152b65dcf1707aa88
“Nilde <nilde.benedetti@gmail.com>
2026-09-15 10:46:07 +0200

    fix: moved master layout structure to app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,35 +1,26 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>@yield("title")</title>
 
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+        {{-- import @vite --}}
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    <!-- Vite -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-
-<body>
-    <div id="app">
-
-
+        <link rel="icon" type="image/svg" href="{{ Vite::asset('resources/images/logos/bloom-logo.svg') }}">
+    </head>
+    <body class="d-flex flex-column min-vh-100">
         @include("partials.header")
 
-        <main class="">
+
+        <main class="flex-grow-1">
             @yield('content')
         </main>
-    </div>
-</body>
 
+        @include("partials.footer")
+    </body>
 </html>
-s
