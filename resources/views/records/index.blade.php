d5f715f5c606d5f5e702fcc6481707b4971954dc
“Nilde <nilde.benedetti@gmail.com>
2026-09-15 18:36:48 +0200

    feat: add back btn to records index

--- resources/views/records/index.blade.php
+++ resources/views/records/index.blade.php
@@ -3,6 +3,12 @@
 @section('title', 'All Records')
 
 @section('content')
+<!-- Back to All btn-->
+    <div class="btn-wrapper px-5 d-flex justify-content-start">
+        <a href="{{ route('dashboard') }}" class="btn bg-light-blue mt-3 text-secondary">
+            Back to Dashboard
+        </a>
+    </div>
     <div class="container py-5">
         <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3 g-3">
             @foreach($records as $record)
