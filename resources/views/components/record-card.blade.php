e302820795502bc07edffbaa10beaf89c3fcf794
“Nilde <nilde.benedetti@gmail.com>
2026-09-15 18:04:33 +0200

    fix: enhance cards spacing and height

--- resources/views/components/record-card.blade.php
+++ resources/views/components/record-card.blade.php
@@ -1,15 +1,17 @@
 @props(['record'])
 
-<div class="card">
-    <div class="card-body">
-        <h4 class="card-title">{{ $record->title }}</h4>
-        <!-- add pills for emotions somewhere here -->
-        <p class="card-subtitle">category</p>
-        <div class="btn-wrapper d-flex justify-content-end">
-            <a href="#" class="btn bg-light-blue mt-3">
-                <i class="bi bi-arrow-right-short"></i>
-            </a>
+<div class="col">
+    <div class="card h-100">
+        <div class="card-body">
+            <h4 class="card-title">{{ $record->title }}</h4>
+            <!-- add pills for emotions somewhere here -->
+            <p class="card-subtitle">category</p>
+            <div class="btn-wrapper d-flex justify-content-end">
+                <a href="#" class="btn bg-light-blue mt-3">
+                    <i class="bi bi-arrow-right-short"></i>
+                </a>
+            </div>
+            
         </div>
-        
     </div>
 </div>
--- resources/views/records/index.blade.php
+++ resources/views/records/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="container py-5">
-        <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3">
+        <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3 g-3">
             @foreach($records as $record)
             <x-record-card :record="$record"></x-record-card>
             @endforeach
