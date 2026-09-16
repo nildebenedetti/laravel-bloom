9ec6d87d4a44b971776754a7ea13b033a518a766
“Nilde <nilde.benedetti@gmail.com>
2026-09-16 17:38:24 +0200

    feat: enhance categories index view with table layout and action buttons

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -3,23 +3,46 @@
 @section('title', 'All Categories')
 
 @section('content')
+    <div class="btns-wrapper container d-flex justify-content-between mt-3">
     <!-- Back to All btn-->
-    <div class="btn-wrapper px-5 d-flex justify-content-start">
-        <a href="{{ route('dashboard') }}" class="btn bg-light-blue mt-3 text-dark">
+    <div class="btn-wrapper d-flex justify-content-start">
+        <a href="{{ route('dashboard') }}" class="btn bg-light-blue text-secondary">
             Back to Dashboard
         </a>
     </div>
     <!-- Resource Interaction Btns-->
-    <div class="btn-wrapper px-5 d-flex justify-content-end">
-        <a href="#" class="btn bg-success mt-3 text-light">
+    <div class="btn-wrapper px-5 d-flex justify-content-end align-items-center">
+        <a href="#" class="btn btn-outline-success">
             Add New
         </a>
     </div>
-    <div class="container py-5">
-        <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3 g-3">
-            @foreach($categories as $category)
-            <p>{{ $category->name }}</p>
-            @endforeach
-        </div>
-    </div>
+</div>
+<div class="container py-3">
+    <table class="table">
+        <thead>
+            <th class="col">#</th>
+            <th class="col">Name</th>
+            <th class="col">Description</th>
+            <th class="col">Actions</th>
+        </thead>
+        <tbody>
+        @foreach ($categories as $category) 
+            <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $category->name }}</td>
+                <td>{{ $category->description }}</td>
+                <td>
+                    <a href="#" class="action-btn btn btn-outline-info"><i class="bi bi-arrow-right"></i></a> 
+                        <a href="#" class="action-btn btn btn-outline-warning"><i class="bi bi-pencil-fill"></i></a>
+                        <button type="button" class="btn btn-outline-danger">
+                                <i class="bi bi-trash3-fill"></i>
+                        </button>   
+                </td>
+            </tr>
+        
+        @endforeach
+        </tbody>
+    </table>
+</div>
+
 @endsection
