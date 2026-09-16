6e96bf3ef615ba566eb4cfaae174e9794e6599ee
“Nilde <nilde.benedetti@gmail.com>
2026-09-16 11:23:55 +0200

    feat: add image storage for record resource

--- app/Http/Controllers/Admin/RecordController.php
+++ app/Http/Controllers/Admin/RecordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Record;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class RecordController extends Controller
 {
@@ -31,12 +32,33 @@ class RecordController extends Controller
      */
     public function store(Request $request)
     {
+        $data = $request->all();
+
         $newRecord = new Record();
 
-        $newRecord->title = $request['title'];
-        $newRecord->description = $request['description'];
-        $newRecord->date = $request['date'];
-        $newRecord->visibility = $request['visibility'];
+        $newRecord->title = $data['title'];
+        $newRecord->description = $data['description'];
+        $newRecord->date = $data['date'];
+        $newRecord->visibility = $data['visibility'];
+
+
+        if(array_key_exists("image", $data)) {
+		
+						// se c`è usiamo il metodo statico Storage::putFile()
+						// con una variabile di appoggio per salvare il path
+						// creato per raggiungere il file
+						
+						$image_path = Storage::putFile('records', $data['image']);
+						// il primo parametro è il nome della cartella dove carichiamo le cose
+						// se la cartella ancora non esiste, viene creata
+						// il secondo parametro è il file, gli passa lárray che vedevamo prima 
+						// con tutte le info di competenza
+                        $newRecord->image_path = $image_path;
+				}
+        if(array_key_exists("image_alt", $data)) {
+            $newRecord->image_alt = $data['image_alt'];
+        }
+		
 
         $newRecord->save();
 
