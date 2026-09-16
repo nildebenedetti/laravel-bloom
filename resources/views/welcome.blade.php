cd9a15231e6ac40d0373801eb38d2390501a3dfd
“Nilde <nilde.benedetti@gmail.com>
2026-09-16 09:13:35 +0200

    feat: add guest check and redirect to dashboard in welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,12 @@
     <div class="page-header container py-4 text-secondary">
         <h2>Bloom Backoffice</h2>
         <p class="pt-2 text-secondary">Access is restricted to our staff only.</p>
-        
-        <div class="d-flex justify-content-center py-5">
+        @guest
+            <div class="d-flex justify-content-center py-5">
                 <a href="{{ route('login') }}" class="btn bg-light-blue text-secondary">
                     Go to Login
                 </a>
-        </div>
+            </div>
+        @endguest        
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,13 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RecordsController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    if (Auth::check()) {
+        return redirect()->route('dashboard');
+    }
     return view('welcome');
 });
 
