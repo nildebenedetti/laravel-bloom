49d187769b6049bb69150f104ed84f492da9f4c2
“Nilde <nilde.benedetti@gmail.com>
2026-09-21 15:58:54 +0200

    feat:use trait for API tokens in user model, add validation ion StoreUserRequest, add logic for storing new user in AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Api\StoreUserRequest;
+use App\Models\User;
 use App\Traits\HttpResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -14,9 +17,24 @@ class AuthController extends Controller
         return 'we are in the login!';
     }
 
-    public function register() {
-        return response()->json(
-            'this is my register response!');
+    public function register(StoreUserRequest $request) { //pass form request as param
+        // if validation is passed
+        $request->validated($request->all());
+
+        // I'LL CREATE A USER
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        // if successful, we use Http response trait to pass repsonse
+        // with entry parameter
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('API Token of' . $user->name)->plainTextToken,
+        ]);
+
     }
 
     public function logout() {
