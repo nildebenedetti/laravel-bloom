d7ce6c482047ecf888a46067b1b7ad5582043b4a
“Nilde <nilde.benedetti@gmail.com>
2026-09-21 18:33:12 +0200

    fix: update triat import and authorization error code in show method of RecordController

--- app/Http/Controllers/Api/RecordController.php
+++ app/Http/Controllers/Api/RecordController.php
@@ -5,10 +5,13 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\RecordResource;
 use App\Models\Record;
+use App\Traits\HttpResponse;
 use Illuminate\Support\Facades\Auth;
 
 class RecordController extends Controller
 {
+    use HttpResponse;
+
         public function index()
             {
                 // only the records of the logged user
@@ -20,7 +23,7 @@ class RecordController extends Controller
         public function show(Record $record) {
 
             if (Auth::user()->id !== $record->user_id) {
-                return $this->error('', 'you are not authorized to access this record', 401);
+                return $this->error('', 'you are not authorized to access this record', 403);
             }
 
             return new RecordResource($record); // parsin into json for response
