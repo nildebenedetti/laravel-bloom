962eac9b0f45f8edd09cd81d85e471a2ca4f021a
“Nilde <nilde.benedetti@gmail.com>
2026-09-23 21:55:36 +0200

    feat: enhance index method to support sorting, searching, and filtering of records

--- app/Http/Controllers/Api/RecordController.php
+++ app/Http/Controllers/Api/RecordController.php
@@ -7,18 +7,41 @@ use App\Http\Requests\Api\StoreRecordRequest;
 use App\Http\Resources\RecordResource;
 use App\Models\Record;
 use App\Traits\HttpResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class RecordController extends Controller
 {
     use HttpResponse;
 
-        public function index()
+        public function index(Request $request)
             {
+                // extract sort order from query string
+                // (es. ?order=asc o ?order=desc)
+                // strtolower is native PHP function === JS .toLowerCase()
+                // order is by def desc unless query strign sets asc
+                $sortOrder = strtolower($request->input('order')) === 'asc' ? 'asc' : 'desc';
+
                 $records = $request->user() // access to auth user instance
                 ->records() // access hasMany relationship method defined in user model
+                // search looks up for values in the column only if search query is present
+                ->when($request->filled('search'), function ($query) use ($request) {
+                    $query->where('title', 'LIKE', '%' . $request->input('search') . '%');
+                })
+                            ->when($request->filled('emotions'), function ($query) use ($request) {
+                    $query->whereHas('emotions', function ($q) use ($request) {
+                        $q->whereIn('emotions.id', (array) $request->emotions);
+                    });
+                })
+                ->when($request->filled('category_id'), function ($query) use ($request) {
+                    $query->where('category_id', $request->category_id);
+                })
+                ->when($request->filled('tier_id'), function ($query) use ($request) {
+                    $query->where('tier_id', $request->tier_id);
+                })
                 ->with(['category', 'tier', 'emotions']) // Eager loads related models data (prevents N+1)
-                ->get(); // executes query
+                ->orderBy('date', $sortOrder)
+                ->paginate(20);
                 // only the records of the logged user
                 return RecordResource::collection($records);
             }
